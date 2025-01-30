c97ff5d5eb72f8f1c027a9db2c2a88b13a9c44ee
tim <147131997+sa111nt@users.noreply.github.com>
2025-01-30 13:07:41 +0100

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -148,8 +148,8 @@ int main(int argc, char ** argv){
     if(argc != 2) error(1, 0, "Required 1 argument");
     auto port = readPort(argv[1]);
 
-    Countries = readCategoriesFromFile("odpowiedzi/panstwa.txt");
-    Cities = readCategoriesFromFile("odpowiedzi/miasta.txt");
+    Countries = readCategoriesFromFile("categories/cities.txt");
+    Cities = readCategoriesFromFile("categories/countries.txt");
 
     serverFd = socket(AF_INET, SOCK_STREAM, 0);
     if(serverFd == -1) error(1, errno, "socket error");
