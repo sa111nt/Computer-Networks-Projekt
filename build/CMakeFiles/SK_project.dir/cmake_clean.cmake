946eba8ba28cfcea55cbe26b43a3375773f142dc
Krzysztof Szmyt <krzysiu@Krzysiu-PC.>
2025-01-30 15:08:17 +0100

    built

---
+++ build/CMakeFiles/SK_project.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SK_project.dir/server.cpp.o"
+  "CMakeFiles/SK_project.dir/server.cpp.o.d"
+  "SK_project"
+  "SK_project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SK_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/SK_project.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for SK_project.
+# This may be replaced when dependencies are built.
